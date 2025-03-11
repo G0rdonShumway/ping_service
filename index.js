4b16fc47dbd54394f916ddfbe156c4eafe620606
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2025-03-12 03:19:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
+const express = require("express");
 const cron = require("node-cron");
 const axios = require("axios");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 const URL_ARRAY = process.env.URL_ARRAY ? process.env.URL_ARRAY.split(",") : [];
+
+app.get("/", (req, res) => {
+  res.send("Cron job is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 cron.schedule(
   "*/5 * * * *",
